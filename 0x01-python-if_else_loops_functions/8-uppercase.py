3ce0c30de61d7767b573fd50dcada8a0cb0c2aab
IMANE EL MARGHANI <96700925+ImaneELMARGHANI@users.noreply.github.com>
2023-11-02 00:54:03 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,9 +6,5 @@ def uppercase(str):
             x -= 32
             c = chr(x)
         print('{}'.format(c), end="")
-<<<<<<< HEAD
+
     print()
-=======
-        
-    print()
->>>>>>> e6872e0a9ca8ce55a863bef1e450e6081f8f0469
