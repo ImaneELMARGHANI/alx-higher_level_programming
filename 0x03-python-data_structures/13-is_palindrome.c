45eec009306aad4bed35e93087bf36415d0d59c4
ImaneELMARGHANI <imanemarghani@gmail.com>
2023-11-17 19:13:59 +0000

    Maybe :|

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,8 +1,41 @@
-#!/usr/bin/python3
+#include "lists.h"
+#include <stddef.h>
 
-def element_at(my_list, idx):
-    list_count = len(my_list)
-    if idx < 0 or idx >= list_count:
-        return None
-    else:
-        return my_list[idx]
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to pointer of the first node of the list
+ * Return: 1 if palindrome, 0 otherwise
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *first = *head;
+	listint_t *last = *head;
+	listint_t *prev = NULL;
+	listint_t *temp;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (last != NULL && last->next != NULL)
+	{
+		last = last->next->next;
+		temp = first->next;
+		first->next = prev;
+		prev = first;
+		first = temp;
+	}
+
+	if (last != NULL)
+		first = first->next;
+
+	while (first != NULL)
+	{
+		if (first->n != prev->n)
+			return (0);
+		first = first->next;
+		prev = prev->next;
+	}
+
+	return (1);
+}
